3756b758900a8c04696c76b3354d378f86455688
Srikar Punna <punna1s@cmich.edu>
2025-05-09 12:50:16 -0400

    Add theme management with custom shadows, types, and theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,20 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 // Import React Query
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from "./theme/theme-provider";
 
 // Create a client
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* Provide the client to your App */}
-    <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    </QueryClientProvider>
+    <ThemeProvider>
+      {/* Provide the client to your App */}
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
